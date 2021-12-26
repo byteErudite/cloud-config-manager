934a63c80e69103a5cf1b3c1f3162c9cc82602ef
byteErudite <raghuvanshivibhu@gmail.com>
2021-12-26 13:56:39 +0530

    Added basic project

---
+++ src/test/java/com/configservice/common/CommonConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configservice.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
