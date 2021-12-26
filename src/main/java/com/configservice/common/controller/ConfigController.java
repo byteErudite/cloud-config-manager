83a2bba853d9099f84d2475fb4bc5f64435205ea
byteErudite <raghuvanshivibhu@gmail.com>
2021-12-26 19:30:31 +0530

    Added basic configuration fetching from properties file

---
+++ src/main/java/com/configservice/common/controller/ConfigController.java
@@ -0,0 +1,42 @@
+package com.configservice.common.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ConfigController {
+
+    @Value("${my.greetings: default greetings}")
+    private String greetingMessage;
+
+    @Value("${message.from: from local}")
+    private String messageFrom;
+
+    @Value("#{${my.value.pairs}}")
+    private Map<String, String> testPairs;
+
+    @Value("${my.name.components}")
+    private List<String> ownerNameComponents;
+
+    @GetMapping("/greeting")
+    public ResponseEntity<String> getPropertiesValue() {
+        return new ResponseEntity<>(greetingMessage + messageFrom, HttpStatus.OK);
+    }
+
+    @GetMapping("/owner")
+    public ResponseEntity<List<String>> getOwnerName() {
+        return new ResponseEntity<>(ownerNameComponents , HttpStatus.OK);
+    }
+
+    @GetMapping("/key/{key}")
+    public ResponseEntity<String> getOwnerName(@PathVariable String key) {
+        return new ResponseEntity<>(testPairs.containsKey(key) ? testPairs.get(key) : "does not exist" , HttpStatus.OK);
+    }
+}
